4c8c9f3c169c6b0f82c0f403da1969816e464cc9
Brandon Eleonora <brandoneleonora38@gmail.com>
2023-03-05 12:14:23 -0500

    complete project

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const employee = {
+   
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const employee2 ={...employee};
+    employee2[key] = value;
+    return employee2;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key]=value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key){
+    const employeeCopy = {...employee};
+    delete employeeCopy.name;   
+    return employeeCopy;
+
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    //console.log(employee.name);
+    delete employee.name;
+    return employee;
+}
